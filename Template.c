7d0c9c021abf1485ceb47c3ed1487fccc438da46
Tetrush <s40813kk@gmail.com>
2022-02-16 03:38:24 +0900

    templateの拡充

--- Template.c
+++ Template.c
@@ -1,16 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
+//配列受け取り　これは配列に入れた2進数を1ビット加算するもの
+int binary_minus(int *s,int i){
+    s[i]+=1;
+    //printf("s[%d]=%d\n",i,s[i]);
+    if(s[i]==2){
+        s[i] = 0;
+        binary_minus(s,i+1);
+    }
+    return 0;
+}
+
+//マージソート
+void merge_sort (int array[], int left, int right) {
+    int i, j, k, mid;
+    int work[10];
+    if (left < right) {
+        mid = (left + right)/2;
+        merge_sort(array, left, mid);
+        merge_sort(array, mid+1, right);
+        for (i = mid; i >= left; i--) { work[i] = array[i]; }
+        for (j = mid+1; j <= right; j++) {
+            work[right-(j-(mid+1))] = array[j];
+        }
+        i = left; j = right;
+        for (k = left; k <= right; k++) {
+            if (work[i] < work[j]) { array[k] = work[i++]; }
+            else                   { array[k] = work[j--]; }
+        }
+    }
+}
+
 int main(void){
     int n;
 
     //二次元配列の宣言
-    int a[5][10] ={"ab","cd","ef","gh"};
+    int s[5][10] ={"ab","cd","ef","gh"};
 
 
     //n個のスペース区切りの入力
     for (int i = 0; i < n; i++)
-        if (scanf("%d", &a[i]) != 1) return 1;
+        if (scanf("%d", &s[i]) != 1) return 1;
     
+    //関数への配列渡し(1次配列)
+    binary_minus(s,0);
+
+    //マージソート(配列,開始位置,終了位置)
+    merge_sort(s, 0, 9); 
+
     return 0;
 }
